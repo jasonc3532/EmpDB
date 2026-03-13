e618e71e1f7ee512f9c91ff640f8b5b73eeb099c
jasonc3532 <jasonc35@uw.edu>
2026-03-13 16:59:21 -0700

    Update Employee.cs
    Added Primary Key "EmailAddress"
    and ToString and ToStringToOutputFile

--- Employee.cs
+++ Employee.cs
@@ -1,9 +1,19 @@
-﻿////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
 //
 //* (C) Copyright 1992-2017 by Deitel & Associates, Inc. and               *
 //* Pearson Education, Inc. All Rights Reserved.                           *
 // Fig. 12.4: Employee.cs
 // Employee abstract base class.
+////////////////////////////////////////////////////////////////////////////
+// TINFO 200 A, Winter 2026
+// UWTacoma SET, Jason Chang
+// 2026-03-13
+///////////////////////////////////////////////////////////////////////////////
+// Change History
+// Date ------- Developer -- Description
+// 2026-03-13  Chang       Employee abstract base class with EmailAddress
+using System;
+
 namespace EmpDB
 {
     public abstract class Employee
@@ -11,23 +21,42 @@ namespace EmpDB
         public string FirstName { get; }
         public string LastName { get; }
         public string SocialSecurityNumber { get; }
+		public string EmailAddress { get; set; } // Primary Key
 
-        // three-parameter constructor
-        public Employee(string firstName, string lastName,
-           string socialSecurityNumber)
+		// three-parameter constructor
+		public Employee(string firstName, string lastName,
+           string socialSecurityNumber, string email)
         {
             FirstName = firstName;
             LastName = lastName;
             SocialSecurityNumber = socialSecurityNumber;
         }
 
-        // return string representation of Employee object, using properties
-        public override string ToString() => $"{FirstName} {LastName}\n" +
-           $"social security number: {SocialSecurityNumber}";
+		// return string representation of Employee object, using properties
+		public override string ToString()
+		{
+			string str = "\n**** Employee Record ****************\n";
+			str += $"First: {FirstName}\n";
+			str += $" Last: {LastName}\n";
+			str += $"Email: {EmailAddress}\n";
+			str += $"  SSN: {SocialSecurityNumber}\n";
+			return str;
+		}
 
-        // abstract method overridden by derived classes
-        public abstract decimal Earnings(); // no implementation here
-    }
+		public virtual string ToStringForOutputFile()
+		{
+			string str = string.Empty;
+			str += $"{FirstName}\n";
+			str += $"{LastName}\n";
+			str += $"{EmailAddress}\n";
+			str += $"{SocialSecurityNumber}";
+			return str;
+		}
+
+		// abstract method overridden by derived classes
+		public abstract decimal Earnings();
+	}
 }
+
 
 
